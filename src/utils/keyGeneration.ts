f9a133cca6e7ffa7b884924528284aa9d8c29999
Greg Brimble <developer@gregbrimble.com>
2019-10-18 09:47:47 +0100

    Fix #14 Use setup-node

--- src/utils/keyGeneration.ts
+++ src/utils/keyGeneration.ts
@@ -2,11 +2,44 @@ import '../metadata'
 
 import { Datastore, Key } from '../Datastore/Datastore'
 import { BaseEntity, ENTITY_METADATA_KEY } from '../Entity/Entity'
-import { CachedValue, ColumnMetadata } from '../Column/Column'
+import { ColumnMetadata, ColumnValue } from '../Column/Column'
 import { getPrimaryColumn } from './columns'
+import { EntityMetadataError, ColumnMetadataError } from './errors'
 
-const getInstanceKey = (instance: BaseEntity): Key =>
-  Reflect.getMetadata(ENTITY_METADATA_KEY, instance.constructor).key
+// TODO: Look at the difference of using the cached Primary Column Value vs. looking it up with getPropertyValue
+
+const getInstanceKey = (instance: BaseEntity): Key => {
+  const entityMetadata = Reflect.getMetadata(
+    ENTITY_METADATA_KEY,
+    instance.constructor
+  )
+
+  if (entityMetadata === undefined) {
+    throw new EntityMetadataError(
+      instance,
+      `Could not find Metadata. Has it been defined yet?`
+    )
+  }
+
+  return entityMetadata.key
+}
+
+const getPrimaryColumnCachedValue = (
+  primaryColumn: ColumnMetadata,
+  instance: BaseEntity
+): ColumnValue => {
+  const cachedValue = primaryColumn.cachedValues.get(instance)
+
+  if (cachedValue === undefined) {
+    throw new ColumnMetadataError(
+      instance,
+      primaryColumn,
+      `Primary Column has no value`
+    )
+  }
+
+  return cachedValue.cachedValue
+}
 
 const getPropertyValue = async (
   instance: BaseEntity,
@@ -24,9 +57,20 @@ export const generatePropertyKey = async (
   const primaryColumn = getPrimaryColumn(instance)
 
   if (primaryColumn) {
-    items.push(
-      (primaryColumn.cachedValues.get(instance) as CachedValue).cachedValue
+    const primaryColumnCachedValue = getPrimaryColumnCachedValue(
+      primaryColumn,
+      instance
     )
+
+    if (primaryColumnCachedValue === undefined) {
+      throw new ColumnMetadataError(
+        instance,
+        primaryColumn,
+        `Primary Column value is undefined`
+      )
+    }
+
+    items.push(primaryColumnCachedValue)
   }
 
   items.push(columnMetadata.key)
@@ -41,7 +85,7 @@ export const generateIndexablePropertyKey = async (
   columnMetadata: ColumnMetadata
 ): Promise<Key> =>
   [
-    Reflect.getMetadata(ENTITY_METADATA_KEY, instance.constructor).key,
+    getInstanceKey(instance),
     columnMetadata.key,
     await getPropertyValue(instance, columnMetadata),
   ].join(datastore.keySeparator)
@@ -49,15 +93,13 @@ export const generateIndexablePropertyKey = async (
 // UUID-HERE
 // or, if singleton, ApplicationConfiguration
 const generateRelationshipKey = async (instance: BaseEntity): Promise<Key> => {
-  const { key } = Reflect.getMetadata(ENTITY_METADATA_KEY, instance.constructor)
-
   const primaryColumn = getPrimaryColumn(instance)
 
   if (primaryColumn) {
     return await getPropertyValue(instance, primaryColumn)
   }
 
-  return key
+  return getInstanceKey(instance)
 }
 
 // Author:UUID-HERE:passport
